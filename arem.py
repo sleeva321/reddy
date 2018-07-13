2cbbe4402b409d5c4c0826912b917136ea852d31
D.siva reddy <40803761+sleeva321@users.noreply.github.com>
2018-07-13 11:49:52 +0530

    Create arem.py

---
+++ arem.py
@@ -0,0 +1,10 @@
+l,u=map(int,input().strip().split())
+for num in range(l,u):
+  temp=num
+  sum=0
+  while temp>0:
+      digit=temp%10
+      sum=sum+digit**3
+      temp=temp//10
+      if sum==num:
+           print (num,end=' ')
